41437cb0f576aceb9708393dfc67d44b04973464
Karthikeya Sharma M <karthikeyasharma@hotmail.com>
2024-08-27 17:14:30 -0400

    updating manager.py and init log.py

---
+++ apex/python/log.py
@@ -0,0 +1,20 @@
+import logging
+import time
+
+class message:
+    def __init__(self):
+        self.start_time = time.time()
+        logging.basicConfig(filename="apex_tacos.log", filemode="w", format="%(name)s - %(levelname)s - %(message)s", level=logging.INFO)
+        logging.info("Starting Logger..")
+
+    def display_info(self, s:str) ->None:
+        logging.info("At t = %0.5f seconds, %s", time.time() - self.start_time, s)
+
+    def display_warning(self, s:str)->None:
+        logging.warning("At t = %0.5f seconds, %s", time.time() - self.start_time, s)
+
+    def display_error(self, s:str)->None:
+        logging.error("At t = %0.5f seconds, %s", time.time() - self.start_time, s)
+
+    def __del__(self):
+        print("Logger file saved as apex_tacos.py under apex/python")
--- apex/python/manager.py
+++ apex/python/manager.py
@@ -1,6 +1,7 @@
 from publisher import Publisher
 from subscriber import Subscriber
 from typing import Dict
+from log import message
 
 class EventManager(Publisher):
     '''
@@ -9,22 +10,27 @@ class EventManager(Publisher):
 
     def __init__(self) -> None:
         super().__init__()
+        self.pub_m = message()
 
     # Add subscriber
     def attach(self, id: int, subscriber: Subscriber) -> None:
         if id not in self._subscribers:
+            self.pub_m.display_info(f"Subscriber {id} doesn't exist!")
             self._subscribers[id] = []
         self._subscribers[id].append(subscriber)
+        self.pub_m.display_info("Subscriber Added!")
     
     # Remove subscriber
     def detach(self, id: int, subscriber: Subscriber) -> None:
         if id in self._subscribers:
             self._subscribers[id].remove(subscriber)
+            self.pub_m.display_info(f"Subscriber {id} Removed!")
     
     # API Endpoint for Upstream
     def forward_event(self, id: int, event: Dict) -> None:
         for subscriber in self._subscribers[id]:
             subscriber.update(event)
+            self.pub_m.display_info(f"Subscriber {id} updated!")
 
     # Notify subscribers of incoming event
     def notify(self, event: Dict) -> None:
@@ -33,3 +39,9 @@ class EventManager(Publisher):
 
         # Forward Event downstream
         self.forward_event(id, event)
+        self.pub_m.display_info(f"Subscriber {id} notified!")
+
+if __name__ == "__main__":
+    s = Subscriber()
+    e = EventManager()
+    e.attach(1,s)
